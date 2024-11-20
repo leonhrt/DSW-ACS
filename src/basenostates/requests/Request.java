2a410bedf425cc1b5fa3c40c2d2bb3b4d5d25a9b
Leonardo Edenak <leonardoedenak@gmail.com>
2024-11-20 22:39:40 +0100

    Request checkstyle done

--- src/basenostates/requests/Request.java
+++ src/basenostates/requests/Request.java
@@ -8,9 +8,22 @@ import org.json.JSONObject;
  * within the system.
  */
 public interface Request {
+  /**
+   * Method that returns the answer as a JSON object.
+   *
+   * @return The information formatted to a JSON object.
+   */
   JSONObject answerToJson();
 
+  /**
+   * Override of the toString method of java.
+   *
+   * @return The string to display.
+   */
   String toString();
 
+  /**
+   * Processes a request send to the specific implementation.
+   */
   void process();
 }
