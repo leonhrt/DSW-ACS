b446bb593fa421cce99688592fcd511a39bea9f2
Leonardo Edenak <leonardoedenak@gmail.com>
2024-11-21 00:32:43 +0100

    Visitor checkstyle done

--- src/basenostates/areas/visitor/Visitor.java
+++ src/basenostates/areas/visitor/Visitor.java
@@ -4,8 +4,28 @@ import basenostates.areas.Partition;
 import basenostates.areas.Space;
 import basenostates.doors.Door;
 
+/**
+ * Interface that declares all the necessary methods for each type of visitor.
+ */
 public interface Visitor {
+  /**
+   * Declaration of visit partition.
+   *
+   * @param partition The partition to visit.
+   */
   void visitPartition(Partition partition);
+
+  /**
+   * Declaration of visit space.
+   *
+   * @param space The space to visit
+   */
   void visitSpace(Space space);
+
+  /**
+   * Declaration of visit door.
+   *
+   * @param door The door to visit.
+   */
   void visitDoor(Door door);
 }
