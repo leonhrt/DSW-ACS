0af03a4aa437d21b7cf3be7fad229d8bdcd6fcc1
Leonardo Edenak <leonardoedenak@gmail.com>
2024-11-20 22:28:57 +0100

    Main checkstyle done

--- src/basenostates/Main.java
+++ src/basenostates/Main.java
@@ -6,10 +6,18 @@ package basenostates;
 import basenostates.areas.DirectoryAreas;
 import basenostates.doors.DirectoryDoors;
 import basenostates.doors.doorstates.Clock;
-import basenostates.users.usergroups.DirectoryUserGroups;
 import basenostates.users.DirectoryUsers;
+import basenostates.users.usergroups.DirectoryUserGroups;
 
+/**
+ * Main class of the program, this is where the execution of the logic starts.
+ */
 public class Main {
+  /**
+   * Main method of the program, this is where the execution starts.
+   *
+   * @param args The arguments passed to the program. Ignored.
+   */
   public static void main(String[] args) {
     DirectoryAreas.makeAreas();           // Create the areas
     DirectoryDoors.makeDoors();           // Create the doors
